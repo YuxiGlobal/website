3668878b801d75dd8516706d91b0fc08799f0a14
Angela Ordoñez <angela.ordonez@yuxiglobal.com>
2017-08-28 11:51:14 -0500

    scroll efect wip

--- src/app/home/home-banner/home-banner.component.ts
+++ src/app/home/home-banner/home-banner.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { MnFullpageService } from "ngx-fullpage";
+import { MnFullpageOptions } from 'ngx-fullpage';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: '[app-home-banner]',
   templateUrl: './home-banner.component.html',
   styleUrls: ['./home-banner.component.scss']
@@ -9,9 +12,10 @@ export class HomeBannerComponent implements OnInit {
   @Input()
   info;
   
-  constructor() { }
+  constructor(private fullpageService: MnFullpageService) { }
 
   ngOnInit() {
+    this.fullpageService.destroy('all');
   }
 
 }
