1b2aa4247aeac632173df81cf36d33b8c946338d
Angela Ordonez <angelitaooo@me.com>
2017-09-30 23:58:04 -0500

    Fix non playing video after changing slides.

--- src/app/home/home-banner/home-banner.component.ts
+++ src/app/home/home-banner/home-banner.component.ts
@@ -1,5 +1,5 @@
 import { IBannerInfo } from 'app/shared/ibanner-info';
-import { Component, OnInit, Input, ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component, Input, ViewEncapsulation, ElementRef, ViewChild, OnChanges } from '@angular/core';
 import { MnFullpageService } from 'ngx-fullpage';
 
 @Component({
@@ -8,20 +8,22 @@ import { MnFullpageService } from 'ngx-fullpage';
   templateUrl: './home-banner.component.html',
   styleUrls: ['./home-banner.component.scss']
 })
-export class HomeBannerComponent implements OnInit {
+export class HomeBannerComponent  implements OnChanges {
 
   readonly CHANGE_TYPE_BREAKPOINT: number = 1024;
   readonly IMAGE_TYPE: string = 'image';
   readonly VIDEO_TYPE: string = 'video';
   @Input() info: IBannerInfo;
+  @Input() isActiveSlide: boolean;
+  @ViewChild('videoElement') videoElement: ElementRef;
 
 
   constructor(private fullpageService: MnFullpageService) {
   }
 
-  ngOnInit() {
-    // this.fullpageService.destroy('all');
+  ngOnChanges() {
+    if (this.isActiveSlide) {
+      this.videoElement.nativeElement.play();
+    }
   }
-
-
 }
