922d5a49c024f21642dc480ad49774c0b1c93cb0
Angela Ordoñez <angelitaooo@me.com>
2017-10-02 10:57:24 -0500

    create advantages section inside services page component and style it for mobile

--- src/app/services-page/services-advantages/services-advantages.component.ts
+++ src/app/services-page/services-advantages/services-advantages.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IAdvantagesCompleteInfo } from 'app/shared/iadvantages-complete-info';
+import { ADVANTAGES_COMPLETE_INFO } from 'app/shared/website-info';
 
 @Component({
   selector: 'app-services-advantages',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services-advantages.component.scss']
 })
 export class ServicesAdvantagesComponent implements OnInit {
+  advantagesCompleteInfo: IAdvantagesCompleteInfo[];
 
-  constructor() { }
+  @Input() info: IAdvantagesCompleteInfo;
+  
+  constructor() {
+    this.advantagesCompleteInfo = ADVANTAGES_COMPLETE_INFO;
+   }
 
   ngOnInit() {
   }
---
+++ src/app/shared/iadvantages-complete-info.ts
@@ -0,0 +1,7 @@
+export interface IAdvantagesCompleteInfo { 
+    id: number;
+    icon: string;
+    title: string;
+    subtitle: string;
+    class: string;
+}
