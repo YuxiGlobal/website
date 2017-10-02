628f3650e3b6c1793ef40ec2dca521cf2872f6f4
Angela Ordoñez <angelitaooo@me.com>
2017-10-02 14:49:51 -0500

    create structure and style the component

--- src/app/services-page/services-page.module.ts
+++ src/app/services-page/services-page.module.ts
@@ -5,13 +5,14 @@ import { ServicesPageComponent } from './services-page.component';
 import { ServicesIntroComponent } from './services-intro/services-intro.component';
 import { ServicesItemsComponent } from './services-items/services-items.component';
 import { ServicesAdvantagesComponent } from './services-advantages/services-advantages.component';
+import { ProgrammingLanguagesComponent } from './programming-languages/programming-languages.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [ServicesPageComponent, ServicesIntroComponent, ServicesItemsComponent, ServicesAdvantagesComponent],
+  declarations: [ServicesPageComponent, ServicesIntroComponent, ServicesItemsComponent, ServicesAdvantagesComponent, ProgrammingLanguagesComponent],
   exports: [ServicesPageComponent]
 })
 export class ServicesPageModule { }
