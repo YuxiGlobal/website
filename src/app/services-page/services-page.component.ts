2f9fde1f93e135b577a80f17f21c75bddcbb8b39
Angela Ordonez <angelitaooo@me.com>
2017-10-06 10:39:33 -0500

    add footer component to services and change get in touch position to fixed

--- src/app/services-page/services-page.component.ts
+++ src/app/services-page/services-page.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ShowOverlayService } from 'app/shared/services/show-overlay.service';
+import { NavigationService } from 'app/shared/services/navigation.service';
 
 @Component({
   selector: 'app-services-page',
   templateUrl: './services-page.component.html',
   styleUrls: ['./services-page.component.scss']
 })
-export class ServicesPageComponent implements OnInit {
+export class ServicesPageComponent {
+  showOverlay = false;
+  activeSlide;
 
-  constructor() { }
+  constructor(
+    private showOverlayService: ShowOverlayService,
+    private navigationService: NavigationService
+  ) {  }
 
-  ngOnInit() {
+  // Adding new class syntax because we were losing context
+  // 'this' was referencing fullPage instead of HomeComponent
+  // https://babeljs.io/docs/plugins/transform-class-properties/
+  onSlideChange = (anchorLink, index) => {
+    this.activeSlide = index;
   }
 
+  openOverlay() {
+    this.showOverlay = !this.showOverlay;
+    this.showOverlayService.preventScroll = !this.showOverlayService.preventScroll;
+  }
+
+  closeOverlay() {
+    this.showOverlay = false;
+    this.showOverlayService.preventScroll = false;
+  }
+
+  resetNavigationValue() {
+    this.navigationService.resetNav = false;
+  }
 }
