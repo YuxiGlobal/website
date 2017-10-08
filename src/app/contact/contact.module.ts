80b06c6eb17ce4d35bffea028b80add3ee030b62
Angela Ordonez <angelitaooo@me.com>
2017-10-07 22:52:46 -0500

    create offices component

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactHeroComponent } from './contact-hero/contact-hero.component';
 import { SharedModule } from './../shared/shared.module';
+import { OfficesComponent } from './offices/offices.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule
   ],
-  declarations: [ContactComponent, ContactHeroComponent]
+  declarations: [ContactComponent, ContactHeroComponent, OfficesComponent]
 })
 export class ContactModule { }
