99253a48d324213701245c3f5d0b21cbec25301c
Angela Ordonez <angelitaooo@me.com>
2017-10-07 21:24:55 -0500

    style hero component

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactHeroComponent } from './contact-hero/contact-hero.component';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [ContactComponent, ContactHeroComponent]
 })
