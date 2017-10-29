f1c8ccf11736e5a9addcfd836ab74c5421ed88ee
Angela Ordonez <angelitaooo@me.com>
2017-10-29 18:53:25 -0500

    create careers module and component

---
+++ src/app/careers/careers.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CareersComponent } from './careers.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CareersComponent]
+})
+export class CareersModule { }
