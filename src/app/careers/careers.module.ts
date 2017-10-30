7a838f9531031ec21e9ee696bb8c48b85c379935
Angela Ordoñez <angelitaooo@me.com>
2017-10-30 09:57:16 -0500

    style in progress

--- src/app/careers/careers.module.ts
+++ src/app/careers/careers.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CareersComponent } from './careers.component';
 import { OurCultureComponent } from './our-culture/our-culture.component';
 import { SharedModule } from './../shared/shared.module';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MatIconModule
   ],
   declarations: [CareersComponent, OurCultureComponent]
 })
