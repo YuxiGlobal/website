ceceea2e8a7df7b9996a3d780ab894dc0833e5b3
Angela Ordoñez <angelitaooo@me.com>
2017-11-02 09:14:56 -0500

    add footer component to careers page

--- src/app/careers/careers.component.ts
+++ src/app/careers/careers.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { OUR_CULTURE_INFO} from 'app/shared/website-info';
 import { IOurCultureInfo } from './../shared/iour-culture-info';
+import { ShowOverlayService } from 'app/shared/services/show-overlay.service';
+import { NavigationService } from 'app/shared/services/navigation.service';
 
 @Component({
   selector: 'app-careers',
   templateUrl: './careers.component.html',
   styleUrls: ['./careers.component.scss']
 })
-export class CareersComponent implements OnInit {
+export class CareersComponent {
   cultureInfo: IOurCultureInfo[];
-  constructor() {
+  showOverlay = false;
+  constructor(
+    private showOverlayService: ShowOverlayService,
+    private navigationService: NavigationService
+  ) {
     this.cultureInfo = OUR_CULTURE_INFO;
   }
 
-  ngOnInit() {
+  openOverlay() {
+    this.showOverlay = !this.showOverlay;
+    this.showOverlayService.preventScroll = !this.showOverlayService.preventScroll;
+  }
+
+  closeOverlay() {
+    this.showOverlay = false;
+    this.showOverlayService.preventScroll = false;
   }
 
 }
