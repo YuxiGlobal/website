fd7d5a9a20836ffc6fd3a7c892439a0d4e68c468
Angela Ordoñez <angelitaooo@me.com>
2018-01-11 16:26:59 -0500

    change hero message in contact

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,16 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
 
-// // use the require method provided by webpack
-// declare const require;
-// // we use the webpack raw-loader to return the content as a string
-// const translations = require(`raw-loader!./locale/messages.es.xlf`);
+// use the require method provided by webpack
+declare const require;
+// we use the webpack raw-loader to return the content as a string
+const translations = require(`raw-loader!./locale/messages.es.xlf`);
 
-// platformBrowserDynamic().bootstrapModule(AppModule, {
-//   providers: [
-//     { provide: TRANSLATIONS, useValue: translations },
-//     { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
-//   ]
-// });
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    { provide: TRANSLATIONS, useValue: translations },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
+  ]
+});
