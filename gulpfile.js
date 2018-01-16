dfc6c5d1afc70714c80f5fac3e61698b1b1631bb
Roger Alvarez <roger.alvarez.c@hotmail.com>
2018-01-16 10:07:39 -0500

    build: including gulp file

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+	runSequence = require('run-sequence'),
+	path = require('path');
+const rootFolder = path.join(__dirname);
+const distFolder = path.join(rootFolder, 'dist');
+const binDistFolder = path.join(rootFolder, 'bin');
+
+// define tasks here
+
+gulp.task('copy:config', function () {
+	console.log("Copying  web.config");  
+	return gulp.src([`${rootFolder}/web.config`])
+		.pipe(gulp.dest(distFolder));
+});
+
+gulp.task('copy:handler', function () {
+	console.log("Copying  binaries");  
+	return gulp.src([`${rootFolder}/bin/**.*.dll`])
+		.pipe(gulp.dest(binDistFolder));
+});
+
+gulp.task('postbuild', function () {
+	runSequence(
+	'copy:handler',
+	'copy:config',
+	function (err) {
+      if (err) {
+        console.log('ERROR:', err.message);
+      } else {
+        console.log('Postbuild actions finished succesfully');
+      }
+    });
+	
+});
+
+gulp.task('default', function(){
+  console.log("Gulp is running!");  
+});
