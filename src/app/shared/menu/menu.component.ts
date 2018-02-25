4edfd57dca18e2943e956d7ef9e0bb8dd1c2e6cf
Angela Ordoñez <angelitaooo@me.com>
2018-02-24 19:21:13 -0500

    Language directive (#38)
    * remove images
    
    * active language is blue

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.scss"]
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  language = true;
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {
+    this.checkLanguage();
   }
 
+  checkLanguage() {
+    const spanish = this.el.nativeElement.querySelector(".spanish");  
+    this.language = window.location.pathname.split("/").includes("es");
+    console.log(this.language);
+  }
 }
